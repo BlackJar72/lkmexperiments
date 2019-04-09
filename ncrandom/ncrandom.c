afea27dc67d3b68e20af2b5f16ac7b3928aeca29
Jared Blackburn (JaredBGreat) <BlackJar72@yahoo.com>
2019-04-08 21:50:10 -0400

    Started on next project, ncrandom

---
+++ ncrandom/ncrandom.c
@@ -0,0 +1,12 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/fs.h>
+#include <linux/time.h>
+#include <linux/uaccess.h>
+#include <asm/uaccess.h>
+
+// See:
+// https://stackoverflow.com/questions/13552536/get-current-time-in-seconds-in-kernel-module
+
+
