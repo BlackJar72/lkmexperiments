6ba46e8ecd58cacd6c969d0f00b7c31a86fada30
Jared Blackburn <BlackJar72@yahoo.com>
2019-04-09 09:56:43 -0400

    ncrandom complete (first original / slash quasi-useful kernel module made)

---
+++ ncrandom/README.md
@@ -0,0 +1,10 @@
+# ncrandom : Non-Cryptographic PRNG Kernel Module
+
+This is my first original (i.e., non-tutorial derived) linux kernel 
+module.  I suppose it could be useful for overwriting incriminating 
+files before deletiong.  Really, though, its a practice project, not 
+something for practical use (this type of rng making more since as 
+part of a user application). Still, it is what I call quasi-useful in 
+that it is not a practical solution to most related problems but 
+could have hypothetical uses (i.e., not just "hello world").
+
--- ncrandom/ncrandom.c
+++ ncrandom/ncrandom.c
@@ -2,17 +2,16 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/fs.h>
+#include <linux/sched.h>
+#include <linux/fs_struct.h>
 #include <linux/time.h>
 #include <linux/uaccess.h>
 #include <asm/uaccess.h>
 
-// See:
-// https://stackoverflow.com/questions/13552536/get-current-time-in-seconds-in-kernel-module
-
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Jared Blackburn");
 MODULE_DESCRIPTION("Generates non-cryptographic random numbers");
-MODULE_VERSION("0.0.1");
+MODULE_VERSION("1.0.0");
 
 #define SHIFT1 19
 #define SHIFT2 31
@@ -20,22 +19,22 @@ MODULE_VERSION("0.0.1");
 #define SHIFT4 7
 #define MULTIPLIER 5443
 #define ADDITION 15485863312338621L
-#define DEVICE_NAME ncrandom
+#define DEVICE_NAME "ncrandom"
 
 static int major_num;
-static int device_open_count = 0;
+static int devCount = 0;
 // I don't think this is needed, but its 
 // the kernel so playing it safe until certain.
-static unsigned long long[3] seedSpace;
+static unsigned long long seedSpace[3];
 static struct timespec startTime;
 static size_t readCounter;
-static int devCount = 0;
 
 
 static int deviceOpen(struct inode*, struct file*);
 static int deviceClose(struct inode*, struct file*);
 static ssize_t deviceRead(struct file*, char*, size_t, loff_t*);
 static ssize_t deviceWrite(struct file*, const char*, size_t, loff_t*);
+static unsigned char generate(void);
 
 
 static struct file_operations fileOps = {
@@ -43,10 +42,10 @@ static struct file_operations fileOps = {
   .write = deviceWrite,
   .open = deviceOpen,
   .release = deviceClose
-}
+};
 
 
-inline static unsigned char generate() {
+static unsigned char generate(void) {
   seedSpace[1] *= MULTIPLIER;
   seedSpace[1] += ADDITION;
   seedSpace[1] ^= seedSpace[1] >> SHIFT1;
@@ -54,22 +53,22 @@ inline static unsigned char generate() {
   seedSpace[1] ^= seedSpace[1] >> SHIFT3;
   seedSpace[1] ^= seedSpace[1] << SHIFT4;
   seedSpace[0] = 0;
-  seedSpace[20 = 0;
+  seedSpace[2] = 0;
   return (unsigned char)((seedSpace[1] >> (((seedSpace[1] >> 8) & 32) + 16)) & 0xff);
 }
 
 
 // Handles reading from the device
 static ssize_t deviceRead(struct file* file, char* buffer, size_t len, loff_t* offset) {
-  for(readCounter = 0; readCounter < len; readCounter++ {
-    put_user(generate(), (buffer[i]);
+  for(readCounter = 0; readCounter < len; readCounter++) {
+    put_user(generate(), buffer + readCounter);
   }
   return readCounter;
 }
 
 
 // Returns error for write attempts (read-only device)
-static ssize_t deviceWrite(struct file* file, const char* buffer, size_t, loff_t* offset) {
+static ssize_t deviceWrite(struct file* file, const char* buffer, size_t len, loff_t* offset) {
   printk(KERN_ALERT "Attempted to write to read-only device ncrandom.");
   return -EINVAL;
 }
@@ -105,7 +104,7 @@ static int __init ncrandomInit(void) {
     printk(KERN_ALERT "Failed to register ncrandom device: %d\n", major_num);
     return major_num;
   } else {
-    printk(KERN_ALERT "Successfull registered ncrandom device as %d.\n", major_num);
+    printk(KERN_INFO "Successfull registered ncrandom device as %d.\n", major_num);
     return 0;
   }
 }
@@ -119,4 +118,4 @@ static void __exit ncrandomExit(void) {
 
 
 module_init(ncrandomInit);
-module_exit(ncrandomInit);
+module_exit(ncrandomExit);
